44428f46259416cc4b5f2076fc1f8446cb9265f8
musheer1999 <musheerahmad.710@gmail.com>
2026-02-02 02:36:07 +0530

    added vercel again16

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,15 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  root: "client",
-
   plugins: [react()],
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "src"),
+      "@shared": path.resolve(__dirname, "../shared"),
+      "@assets": path.resolve(__dirname, "../attached_assets"),
     },
   },
 
